7f336ecf7b11745923282c33bb9a10a23c031e38
Luca <luca.almond.j@gmail.com>
2024-09-10 20:27:48 +0930

    New Map, Music and Main Menu Fixes (Tons of assets)

--- Luca-FPS/Assets/Scripts/GameManager/GameManager.cs
+++ Luca-FPS/Assets/Scripts/GameManager/GameManager.cs
@@ -4,11 +4,12 @@ using System.Runtime.CompilerServices;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using UnityEditor;
+using Unity.VisualScripting;
 
 public class GameManager : MonoBehaviour
 {
     public HighScores highScores;
-    //reference to the overlay text to display winning text, etc.
     public TextMeshProUGUI messageText;
     public TextMeshProUGUI timerText;
     public TextMeshProUGUI scoreText;
@@ -23,12 +24,14 @@ public class GameManager : MonoBehaviour
     public GameObject player;
     public Camera worldCamera;
 
+    public AudioSource Menu;
+    public AudioSource Playing;
+
     public float startTimerAmount = 3;
     private float startTimer;
 
     public float targetActivateTimerAmount = 1;
     private float targetActivateTimer;
-    
 
     public float gametimerAmount = 60;
     private float gameTimer;
@@ -42,25 +45,28 @@ public class GameManager : MonoBehaviour
         Gameover
     };
 
-    GameState gameState;
+    public GameState gameState;
     public GameState State { get { return gameState; } }
-
-
 
     private void Awake()
     {
         gameState = GameState.Gameover;
+        Menu.Play();  // Start with menu music
+        Playing.Stop();  // Ensure the game music isn't playing at the start
     }
+
     private void Start()
     {
         Cursor.lockState = CursorLockMode.Confined;
         player.SetActive(false);
         worldCamera.gameObject.SetActive(true);
+
         for (int i = 0; i < targets.Length; i++)
         {
             targets[i].gameManager = this;
             targets[i].gameObject.SetActive(false);
         }
+
         startTimer = startTimerAmount;
         messageText.text = "Press Enter to Start";
         timerText.text = " ";
@@ -69,33 +75,32 @@ public class GameManager : MonoBehaviour
         highScorePanel.gameObject.SetActive(false);
         newGameButton.gameObject.SetActive(true);
         highScoresButton.gameObject.SetActive(true);
-
     }
+
     private void Update()
     {
-
+        if (Input.GetKeyUp(KeyCode.Escape))
         {
-            if (Input.GetKeyUp(KeyCode.Escape))
-            {
-                Application.Quit();
-            }
+            Application.Quit();
+        }
 
-            switch (gameState)
-            {
-                case GameState.Start:
-                    GameStateStart();
-                    break;
-                case GameState.Playing:
-                    GameStatePlaying();
-                    break;
-                case GameState.Gameover:
-                    GameStateGameOver();
-                    break;
-            }
+        switch (gameState)
+        {
+            case GameState.Start:
+                GameStateStart();
+                break;
+            case GameState.Playing:
+                GameStatePlaying();
+                break;
+            case GameState.Gameover:
+                GameStateGameOver();
+                break;
         }
     }
+
     private void GameStateStart()
     {
+        highScorePanel.gameObject.SetActive(false);
         startTimer -= Time.deltaTime;
 
         messageText.text = "Get Ready " + (int)(startTimer + 1);
@@ -111,34 +116,45 @@ public class GameManager : MonoBehaviour
 
             player.SetActive(true);
             worldCamera.gameObject.SetActive(false);
+
+            Menu.Stop();  // Stop menu music
+            Playing.Play();  // Start game music
         }
     }
 
-
     private void GameStatePlaying()
     {
+        highScorePanel.gameObject.SetActive(false);
+        newGameButton.gameObject.SetActive(false);
         gameTimer -= Time.deltaTime;
+
         int seconds = Mathf.RoundToInt(gameTimer);
-        timerText.text = string.Format("Time: {0:D2}:{1:D2}",
-            (seconds / 60), (seconds % 60));
+        timerText.text = string.Format("Time: {0:D2}:{1:D2}", (seconds / 60), (seconds % 60));
+
         if (gameTimer <= 0)
         {
             Cursor.lockState = CursorLockMode.Confined;
             messageText.text = "Game Over! Score: " + score + "\nPress enter to play again.";
             gameState = GameState.Gameover;
+
             player.SetActive(false);
             worldCamera.gameObject.SetActive(true);
+
             for (int i = 0; i < targets.Length; i++)
             {
                 targets[i].gameObject.SetActive(false);
             }
+
             highScores.AddScore(score);
             highScores.SaveScoresToFile();
+
             newGameButton.gameObject.SetActive(false);
             highScoresButton.gameObject.SetActive(true);
 
+            Playing.Stop();  // Stop game music
+            Menu.Play();  // Return to menu music
         }
-        //timer before activating target
+
         targetActivateTimer -= Time.deltaTime;
         if (targetActivateTimer <= 0)
         {
@@ -146,6 +162,7 @@ public class GameManager : MonoBehaviour
             targetActivateTimer = targetActivateTimerAmount;
         }
     }
+
     private void GameStateGameOver()
     {
         if (Input.GetKeyUp(KeyCode.Return))
@@ -158,14 +175,13 @@ public class GameManager : MonoBehaviour
 
     private void ActivateRandomTarget()
     {
-        int randomindex = Random.Range(0, targets.Length);
-        targets[randomindex].gameObject.SetActive(true);
+        int randomIndex = Random.Range(0, targets.Length);
+        targets[randomIndex].gameObject.SetActive(true);
     }
-    
+
     public void OnNewGame()
     {
         gameState = GameState.Start;
-
     }
 
     public void OnHighScores()
@@ -176,7 +192,6 @@ public class GameManager : MonoBehaviour
         highScorePanel.gameObject.SetActive(true);
         newGameButton.gameObject.SetActive(true);
 
-
         string text = "";
         for (int i = 0; i < highScores.scores.Length; i++)
         {
@@ -184,11 +199,10 @@ public class GameManager : MonoBehaviour
         }
         highScoresText.text = text;
     }
+
     public void AddScore(int points)
     {
         score += points;
         scoreText.text = "Score: " + score;
     }
 }
-
-
